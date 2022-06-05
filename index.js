2d725520d73294256fecb35e6ac0d4e753286a2f
Felipe <fmagesty@gmail.com>
2022-06-05 16:39:00 -0300

    enh:add initial eslint/prettier config and format

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import express from 'express'
-const app = express()
-const port = 3000
+import express from "express";
+const app = express();
+const port = 3000;
 
 const apiRequester = async () => {
-  app.get('/', (req, res) => {
-    res.send('This is some request sent by express\n')
-  })
+  app.get("/", (req, res) => {
+    res.send("This is some request sent by express\n");
+  });
   app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-}
+    console.log(`Example app listening on port ${port}`);
+  });
+};
 
-apiRequester()
+apiRequester();
