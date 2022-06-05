a805f23f990a02627f24015adc1c4b422ebcf43b
Felipe <fmagesty@gmail.com>
2022-06-05 16:22:59 -0300

    fea:add initial request

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-    import express from 'express'
-    const app = express()
-    const port = 3000
+import express from 'express'
+const app = express()
+const port = 3000
 
-    app.get('/', (req, res) => {
-      res.send('Hello World!\n')
-    })
+const apiRequester = async () => {
+  app.get('/', (req, res) => {
+    res.send('This is some request sent by express\n')
+  })
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+}
 
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`)
-    })
+apiRequester()
+apiRequester()
+apiRequester()
