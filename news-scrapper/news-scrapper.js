d9b03e833b41548d7d76ef0afc5939aee606cb92
Felipe <fmagesty@gmail.com>
2022-06-15 20:19:48 -0300

    fea:add initial news scrapper

--- news-scrapper/news-scrapper.js
+++ news-scrapper/news-scrapper.js
@@ -1,10 +1,11 @@
 import express from "express";
 import axios from "axios";
+// import * as cheerio from "cheerio";
 
 const app = express();
 const PORT = process.env.port || 3000;
+// TODO: add multiple websites (have to check how cheerio web scrapes each one first)
 const website = "https://thehackernews.com/";
-
 try {
   axios(website).then((response) => {
     const html = response.data;
